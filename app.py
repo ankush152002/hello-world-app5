999667019a1a9bdf776fa868ed4d86f5916d0e3a
Ankush Singh <98583838+ankush152002@users.noreply.github.com>
2024-10-17 18:40:19 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,20 @@
+import psutil
+from flask import Flask, render_template, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/metrics")
+def metrics():
+    cpu_metric = psutil.cpu_percent()
+    mem_metric = psutil.virtual_memory().percent
+    message = None
+    if mem_metric > 40:
+        message = "High Memory and CPU Usage Detected"
+    return jsonify(cpu_metric=cpu_metric, mem_metric=mem_metric, message=message)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')

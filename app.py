13a3c313d2c8507924e36d8348c847bfd94df9d1
Ankush Singh <98583838+ankush152002@users.noreply.github.com>
2024-10-17 19:07:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,4 +17,4 @@ def metrics():
     return jsonify(cpu_metric=cpu_metric, mem_metric=mem_metric, message=message)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0',port=3004)
